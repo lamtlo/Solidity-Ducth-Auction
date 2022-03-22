d5c34e4f887abfbb608da11e4568c4a6c1dfb9da
Lam Lo <lamtlo820@gmail.com>
2022-03-22 15:05:09 -0700

    Added a complete deployment script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,9 @@
-# This script is not finished as prerequisites to deploy are not met
-from traceback import print_tb
 from brownie import accounts, SimpleToken, DutchAuction
 
 
 def main():
     acct = accounts.load("deployment_account")
     token = SimpleToken.deploy({"from": acct})
-    auction = DutchAuction.deploy(1e9, 1000, token.address, {"from": acct})
+    auction = DutchAuction.deploy(1e9, 1000, 10, token.address, {"from": acct})
     token.approve(auction.address, 5, {"from": acct})
     return auction
